d3b2bb92c031fd46e4dc69167ad4386f5ef21ff6
Rustam Mamedov <rmamedov@swivl.com>
2024-04-10 16:38:18 +0300

    Setup project and add missing documentation

---
+++ lib/my_app.dart
@@ -0,0 +1,42 @@
+
+import 'package:color_generator/my_home_page.dart';
+import 'package:flutter/material.dart';
+
+/// The root widget of the application.
+///
+/// This widget serves as the entry point of the application. It initializes
+/// and configures the overall appearance and behavior of the application.
+class MyApp extends StatelessWidget {
+  /// Creates a new instance of [MyApp].
+  ///
+  /// The [key] parameter is an optional parameter that can be used to
+  /// uniquely identify this widget in the widget tree.
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
