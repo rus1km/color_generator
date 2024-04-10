5f9028291ba8de987fdb35f4d7ede0352f2bfa9e
Rustam Mamedov <rmamedov@swivl.com>
2024-04-10 18:34:40 +0300

    [CG-1] Implement UI for color change page

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Hello there'),
     );
   }
 }
